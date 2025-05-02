6f76f29a995428d0857e202c287a14c5caff9475
sanni-sudo <sanna.nilsson@utb.ecutbildning.se>
2025-05-02 16:48:52 +0200

    Uppdaterade uppgifter

--- sec_watch.py
+++ sec_watch.py
@@ -20,4 +20,53 @@ import re           # Reguljära uttryck som används för att söka efter nycke
 LOG_DIR = "/home/testkali/KursRepo/Test_Monitor_Dir"
 LOG_FILE = os.path.join(LOG_DIR, "network_traffic.log")
 REPORT_FILE = os.path.join(LOG_DIR, "attack_report.csv")
-KEYWORDS = []
+KEYWORDS = ["time", "src-ip", "dest.ip", "port", "protoc"]
+
+# Funktioner
+# Funktion för att konfigurera loggning
+def setup_logging():
+    logging.basicConfig(
+        filename=REPORT_FILE,
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+# Funktion för att loga meddelanden
+def log_message(level, message):
+    if level == "INFO":
+        logging.info(message)
+    elif level == "WARNING":
+        logging.warning(message)
+    elif level == "ERROR":
+        logging.error(message)
+
+# Validera loggkatalog
+    log_message("INFO", "Startar nätverksövervakning...")
+if not os.path.exists(LOG_DIR):
+    log_message("ERROR", f"Katalogen {LOG_DIR} finns inte.")
+    print(f"ERROR: Katalogen {LOG_DIR} finns inte!")
+    exit(1)
+
+# Skannar loggfilen för nyckelord
+
+
+
+
+#except FileNotFoundError:
+#log_message("ERROR", f"Kunde inte hitta en fil i {LOG_DIR}.")
+#print(f"ERROR: Kunde inte hitta en fil!")
+#exit(1)
+
+#except PermissionError:
+#    log_message("ERROR", f"Tillstånd nekades för en fil i {LOG_DIR}.")
+#    print(f"ERROR: Tillstånd nekades!")
+
+#except Exception as e:
+#   log_message("ERROR", f"Oväntat fel: {str(e)}")
+#   exit(1)
+
+# Konsolsammanfattning
+
+#print(f"Övervakning av nätverkstrafik klar: {time.strtime('%Y-%m-%d %H:%M:%S')}")
+#print(f"Rapport skickad: {RAPPORT_FILE}")
+
