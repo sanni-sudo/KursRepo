3007237aab90d88fd615a3dc99d71dd1bc607455
sanni-sudo <sanna.nilsson@utb.ecutbildning.se>
2025-05-08 09:12:30 +0200

    Uppdaterade filer

--- sec_watch_1.py
+++ sec_watch_1.py
@@ -1,17 +1,19 @@
-# sec_watch_1.py -> Nytt försök - Övervakar nätverkstrafik i realtid 
-# 
+# sec_watch_1.py -> Nytt försök av Slutuppgiften - Övervakar nätverkstrafik i realtid
 # Skapat av: Sanna Nilsson, 2 Maj 2025
-# Syfte: Bygga ett Python-program som kan upptäcka attacker 
-# mot nätverket i realtid och kan vidta åtgärder vid 
-# misstänkta attacker.
-
-# Krav
-# Läsa en dynamisk loggfil (network_traffic.log) 
-# Analyserar trafiken 
-# Skickar e-post varningar till en administratör
-# Genererar en CSV-rapport
+# Syfte: Bygga ett Python-program som kan upptäcka attacker mot nätverket i 
+# realtid och kan vidta åtgärder vid misstänkta attacker.
+  
+# Krav:
+# -> Läsa loggfilen med tid, källa, destination, port och protokoll
+# -> Flagga anslutningar till ovanliga portar och IPn med fler än 100 anslutningar
+# -> Skicka e-postvarningar vid upptäckta händelser
+# -> Spara en CSV-rapport med fynden
+# -> Simulera UFW-blockering
+# -> Logga allt
+# -> Kör i en slinga för att repetera
  
 # Modulimportering-----------------------------
+
 import os
 import logging
 import time
@@ -26,6 +28,7 @@ import subprocess
 
 LOG_DIR = "/home/testkali/KursRepo/Test_Monitor_Dir"
 LOG_FILE = os.path.join(LOG_DIR, "monitor.log")
+CSV_FILE = os.path.join(LOG_DIR, "attack_report.csv")
 TRAFFIC_LOG = os.path.join(LOG_DIR, "network_traffic.log")
 # För intern/test e-post
 SMTP_SERVER = "localhost"
@@ -76,6 +79,17 @@ def extract_timestamp(line):
         return datetime.strptime(parts[0].strip(), "%Y-%m-%d %H:%M:%S")
     return None
 
+def cleanup():
+    # Rensa loggfilen och CSV-filen
+    # Om filen inte finns skapas den
+    with open(LOG_FILE, "w") as file:
+        print(f"Rensar loggfilen: {LOG_FILE}")
+        log_message("INFO", f"Rensar loggfilen: {LOG_FILE}")
+
+    with open(CSV_FILE, "w") as file:
+        print(f"Rensar CSV-filen: {CSV_FILE}")
+        log_message("INFO", f"Rensar CSV-filen: {CSV_FILE}")
+
 def block_ufw(IP):
     try:
         # Kör kommandot med sudo
@@ -91,8 +105,17 @@ def block_ufw(IP):
         print(f"Kunde inte blockera IP {IP}: {e.stderr}")
         log_message("ERROR" f"Kunde inte blockera IP {IP}: {e.stderr}")
 
+    try:
+        result = subprocess.run(["sudo", "ufw", "deny"], capture_output=True, text=True)
+        log_message("INFO", "Blockerade IP med UFW")
+    except:
+        log_message("ERROR", "Kunde inte blockera IP")
+        print("Kunde inte köra block-kommandot!")  
+       
+
 # Validering---------------------------
 setup_logging()
+cleanup()
 log_message("INFO", "Startar nätverkskontrollen...")
 
 if not os.path.exists(TRAFFIC_LOG):
@@ -102,15 +125,18 @@ if not os.path.exists(TRAFFIC_LOG):
 
 # Huvudlogik-----------------------------
 
-#suspicious_ports = []
-
 try:
     connections = {}
     with open(TRAFFIC_LOG, "r") as file:
         start_time = None
+        ok_con =  0
+        susp_port = 0
+        many_con = []
+        lines = 0
         for line in file:
             parts = line.strip().split(",")
             current_time = extract_timestamp(line)
+            lines += 1
             if not current_time:
                 continue
             if not start_time:
@@ -119,9 +145,31 @@ try:
             if (current_time - start_time) >= timedelta(minutes=5):
                 print("5 minuter har passerat.")
                 start_time = current_time
-                print("Pausar 10 sekunder. Ny starttid: ", start_time)
-                time.sleep(10)
+                log_message("INFO", f"Pausar 20 sekunder. Ny starttid:  {start_time}")
+                log_message("INFO", "Statistik för de senaste 5 minuterna")
+                log_message("INFO", f"Antal anslutningar: {lines}")
+                log_message("INFO", f"Antal OK anslutningar: {ok_con}")
+                log_message("INFO", f"Antal misstänkta portar: {susp_port}")
 
+                print(f"Pausar 20 sekunder. Ny starttid: ", {start_time})
+                print("Statistik för de senaste 5 minuterna")
+                print(f"Antal anslutningar: {lines}")
+                print(f"Antal OK anslutningar: {ok_con}")
+                print(f"Antal misstänkta portar: {susp_port}")
+
+                unique_ips = set(many_con)
+                for ip in unique_ips:
+                    print(f"IP {ip} har fler än 100 anslutningar")
+                    log_message("WARNING", f"IP {ip} har fler än 100 anslutningar")
+                with open(CSV_FILE, "a") as file:
+                    file.write("IP, Antal\n")
+                    for ip, count in connections.items():
+                        file.write(ip + "," + str(count) + "\n")  
+                        file.flush()          
+                ok_con = 0
+                susp_port = 0
+                many_con = []
+                time.sleep(20)
 
             if len(parts) >= 4:
                 port = int(parts[3].strip())
@@ -129,51 +177,35 @@ try:
                     message = f"Hittat ovanlig port {port} i raden: {line.strip()}"
                     log_message("WARNING", message)
                     print("WARNING: ", message)
-
-#                suspicious_ports.append(f"Port {port} upptäcktes i följande rad:\n{line.strip()}")
-
+                    susp_port += 1
                     send_email_alert(
                     subject="IDS-varning: Misstänkt port upptäckt",
-                    body=f"Port {port} upptäcktes i följande rad:\n{line.strip()}"
-         )                                                                                     
+                    body=f"Port {port} upptäcktes i följande rad:\n{line.strip()}")                   
+                else:
+                    ok_con += 1                                                                                      
             
-                # Låtsas att filen skriver tid, adress               
-                if len(parts) >=2:
-                    ip = parts[1]
-                    connections[ip] = connections.get(ip, 0) + 1
+            # Låtsas att filen skriver tid, adress               
+            if len(parts) >=2:
+                ip = parts[1]
+                connections[ip] = connections.get(ip, 0) + 1
 
-                    if connections[ip] > 100:
-                        message = "IP" + ip + " har för många anslutningar"
-                        log_message("WARNING", message)
-                        block_ufw(ip.strip())
-    with open("/home/testkali/KursRepo/Test_Monitor_Dir/suspicious_ips.csv", "w") as file:
-        file.write("IP, Antal\n")
-        for ip, count in connections.items():
-            file.write(ip + "," + str(count) + "\n")
+                if connections[ip] > 100:
+                    many_con.append(ip)
+                    message = "IP" + ip + " har för många anslutningar"
+                    log_message("WARNING", message)
+                    print("WARNING:", message)
+                    block_ufw(ip.strip())
+                    send_email_alert(
+                    subject="IDS-varning: Misstänkt många anslutningar upptäckt",
+                    body=f"IP-adress: {ip} har fler än 100 anslutingar under senaste 5 minuters perioden. Blockerar med UFW")                   
 
     log_message("INFO", "Sparade rapporten")
     print("IP-rapport har sparats på systemet!")
-    
-# Send a summary email with all suspicious ports
-#    if suspicious_ports:
-#        log_message("DEBUG", f"Sammanfattning av misstänkta portar: {suspicious_ports}")
-#        summary_body = "Sammanfattning av upptäckta misstänkta portar:\n\n" + "\n".join(suspicious_ports)
-#        send_email_alert(
-#            subject="IDS-varning: Sammanfattning av misstänkta portar",
-#            body=summary_body
-#    )
-#        log_message("INFO", "Sammanfattningsmail skickat.")
-#    else:
-#        log_message("INFO", "Inga misstänkta portar att rapportera.")
-#        )
-
     log_message("INFO", "Klar med kontrollen")
     print("Klar med  kontroll av loggfilen")
     log_message("INFO", "Loggfilen kontrollerad")
-
 except Exception as e:
     log_message("ERROR", "Något gick fel!")
     exit(1)
-
 
 
--- sec_watch_1_jj.py
+++ sec_watch_1_jj.py
@@ -1,24 +1,19 @@
-# sec_watch_1.py -> Nytt försök av Slutuppgiften i Python-skriptet
-# Skapat av: Sanna Nilsson, 5 maj 2025
-
-# Syfte:
-# Läsa Nätverkslogg
-# Flagga vid misstänkt portanvändning
-# Räkna IP-anslutningar
-# Skicka e-post
-# Spara rapport
-# Simulera IP-blockering
-# 
-# Kravbild:
+# sec_watch_1.py -> Nytt försök av Slutuppgiften - Övervakar nätverkstrafik i realtid
+# Skapat av: Sanna Nilsson, 2 Maj 2025
+# Syfte: Bygga ett Python-program som kan upptäcka attacker mot nätverket i 
+# realtid och kan vidta åtgärder vid misstänkta attacker.
+  
+# Krav:
 # -> Läsa loggfilen med tid, källa, destination, port och protokoll
-# -> Flagga anslutningar till port 8080 och/eller IPn med fler än 5 anslutningar
+# -> Flagga anslutningar till ovanliga portar och IPn med fler än 100 anslutningar
 # -> Skicka e-postvarningar vid upptäckta händelser
 # -> Spara en CSV-rapport med fynden
 # -> Simulera UFW-blockering
 # -> Logga allt
 # -> Kör i en slinga för att repetera
-# 
+ 
 # Modulimportering-----------------------------
+
 import os
 import logging
 import time
@@ -33,6 +28,7 @@ import subprocess
 
 LOG_DIR = "/home/testkali/KursRepo/Test_Monitor_Dir"
 LOG_FILE = os.path.join(LOG_DIR, "monitor.log")
+CSV_FILE = os.path.join(LOG_DIR, "attack_report.csv")
 TRAFFIC_LOG = os.path.join(LOG_DIR, "network_traffic.log")
 # För intern/test e-post
 SMTP_SERVER = "localhost"
@@ -83,12 +79,24 @@ def extract_timestamp(line):
         return datetime.strptime(parts[0].strip(), "%Y-%m-%d %H:%M:%S")
     return None
 
+def cleanup():
+    # Rensa loggfilen och CSV-filen
+    # Om filen inte finns skapas den
+    with open(LOG_FILE, "w") as file:
+        print(f"Rensar loggfilen: {LOG_FILE}")
+        log_message("INFO", f"Rensar loggfilen: {LOG_FILE}")
+
+    with open(CSV_FILE, "w") as file:
+        print(f"Rensar CSV-filen: {CSV_FILE}")
+        log_message("INFO", f"Rensar CSV-filen: {CSV_FILE}")
+
+
 def block_ufw(IP):
     try:
         # Kör kommandot med sudo
         result = subprocess.run(
             ["sudo", "ufw", "deny", "from", IP],
-            capture_output=True,
+           capture_output=True,
             text=True,
             check=True
         )
@@ -97,16 +105,17 @@ def block_ufw(IP):
     except subprocess.CalledProcessError as e:
         print(f"Kunde inte blockera IP {IP}: {e.stderr}")
         log_message("ERROR" f"Kunde inte blockera IP {IP}: {e.stderr}")
-#    try:
-#        result = subprocess.run(["sudo", "ufw", "deny"], capture_output=True, text=True)
-#        log_message("INFO", "Blockerade IP med UFW")
-#    except:
-#        log_message("ERROR", "Kunde inte blockera IP")
-#        print("Kunde inte köra block-kommandot!")
+    try:
+        result = subprocess.run(["sudo", "ufw", "deny"], capture_output=True, text=True)
+        log_message("INFO", "Blockerade IP med UFW")
+    except:
+        log_message("ERROR", "Kunde inte blockera IP")
+        print("Kunde inte köra block-kommandot!")
 
 
 # Validering---------------------------
 setup_logging()
+cleanup()
 log_message("INFO", "Startar nätverkskontrollen...")
 
 if not os.path.exists(TRAFFIC_LOG):
@@ -119,9 +128,14 @@ try:
     connections = {}
     with open(TRAFFIC_LOG, "r") as file:
         start_time = None
+        ok_con =  0
+        susp_port = 0
+        many_con = []
+        lines = 0
         for line in file:
             parts = line.strip().split(",")
             current_time = extract_timestamp(line)
+            lines += 1
             if not current_time:
                 continue
             if not start_time:
@@ -130,8 +144,30 @@ try:
             if (current_time - start_time) >= timedelta(minutes=5):
                 print("5 minuter har passerat.")
                 start_time = current_time
-                print("Pausar 10 sekunder. Ny starttid: ", start_time)
-                time.sleep(10)
+                log_message("INFO", f"Pausar 20 sekunder. Ny starttid:  {start_time}")
+                log_message("INFO", "Statistik för de senaste 5 minuterna")
+                log_message("INFO", f"Antal anslutningar: {lines}")
+                log_message("INFO", f"Antal OK anslutningar: {ok_con}")
+                log_message("INFO", f"Antal misstänkta portar: {susp_port}")
+            
+                print(f"Pausar 20 sekunder. Ny starttid: ", {start_time})
+                print("Statistik för de senaste 5 minuterna")
+                print(f"Antal anslutningar: {lines}")
+                print(f"Antal OK anslutningar: {ok_con}")
+                print(f"Antal misstänkta portar: {susp_port}")
+                unique_ips = set(many_con)
+                for ip in unique_ips:
+                    print(f"IP {ip} har fler än 100 anslutningar")
+                    log_message("WARNING", f"IP {ip} har fler än 100 anslutningar")
+                with open(CSV_FILE, "a") as file:
+                    file.write("IP, Antal\n")
+                    for ip, count in connections.items():
+                        file.write(ip + "," + str(count) + "\n")  
+                        file.flush()          
+                ok_con = 0
+                susp_port = 0
+                many_con = []
+                time.sleep(20)
                     
             if len(parts) >= 4:
                 port = int(parts[3].strip())
@@ -139,11 +175,12 @@ try:
                     message = f"Hittat ovanlig port {port} i raden: {line.strip()}"
                     log_message("WARNING", message)
                     print("WARNING: ", message)
-                    
+                    susp_port += 1
                     send_email_alert(
                     subject="IDS-varning: Misstänkt port upptäckt",
                     body=f"Port {port} upptäcktes i följande rad:\n{line.strip()}")                   
-                        
+                else:
+                    ok_con += 1        
                             
             # Låtsas att filen skriver tid, adress               
             if len(parts) >=2:
@@ -151,14 +188,14 @@ try:
                 connections[ip] = connections.get(ip, 0) + 1
 
                 if connections[ip] > 100:
+                    many_con.append(ip)
                     message = "IP" + ip + " har för många anslutningar"
                     log_message("WARNING", message)
                     print("WARNING:", message)
                     block_ufw(ip.strip())
-    with open("/home/testkali/KursRepo/Test_Monitor_Dir/suspicious_ips.csv", "w") as file:
-        file.write("IP, Antal\n")
-        for ip, count in connections.items():
-            file.write(ip + "," + str(count) + "\n")
+                    send_email_alert(
+                    subject="IDS-varning: Misstänkt många anslutningar upptäckt",
+                    body=f"IP-adress: {ip} har fler än 100 anslutingar under senaste 5 minuters perioden. Blockerar med UFW")                   
 
     log_message("INFO", "Sparade rapporten")
     print("IP-rapport har sparats på systemet!")
